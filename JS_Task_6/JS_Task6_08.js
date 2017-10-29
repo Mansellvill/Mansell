bb19e1386488c2cc2b8b668719e6d7935ed36f7a
Vladimir Samodelov <vladimir.samodelov19@gmail.com>
2017-10-29 22:11:09 +0300

    New tasks

--- JS_Task_6/JS_Task6_08.js
+++
@@ -1,8 +0,0 @@
-function slasher(arr, howMany) {
-    return arr.slice(howMany);
-
-  }
-  
-  console.log(slasher([1, 2, 3], 2));
-  console.log(slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5));
-  console.log(slasher(["burgers", "fries", "shake"], 1));
