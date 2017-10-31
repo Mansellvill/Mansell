900070bcdd201b81bafd64f4b7e3c2c445222397
Vladimir Samodelov <vladimir.samodelov19@gmail.com>
2017-10-31 17:02:49 +0300

    Delite tast8

--- JS_Task_6/JS_Task6_15_(Sorted Union Incomplete ).js
+++
@@ -1,9 +0,0 @@
-function uniteUnique(arr) {
-    var newArr = [].concat(...arguments);
-    return [... new Set (newArr)];
-  }
-
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
-console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]));
-console.log(uniteUnique([1, 2, 3], [5, 2, 1]));
-console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
